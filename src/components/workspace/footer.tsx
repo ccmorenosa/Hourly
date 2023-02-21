19bc7462005551f59dad8d17bddff9638875937a
Catalina Sarria <cimorenos@unal.edu.co>
2023-02-21 01:07:58 -0500

    Add status field in footer

--- src/components/workspace/footer.tsx
+++ src/components/workspace/footer.tsx
@@ -30,11 +30,24 @@ class WorkspaceFooter extends React.Component<{}, {}> {
     render(): React.ReactNode {
         /** @typedef {string} - Class for the main div. */
         let footerClass = (
-            "h-5 bg-purple-500 text-xs flex"
+            "h-5 bg-purple-800 text-xs flex text-gray-0 font-semibold"
+        );
+
+        /** @typedef {string} - Class for the main div. */
+        let statusClass = (
+            "h-full flex flex-col px-5"
         );
 
         return (
             <div className={footerClass}>
+
+                <div className={statusClass}>
+
+                    <div id="Status" className="my-auto">
+                        Idle
+                    </div>
+
+                </div>
 
             </div>
         );
