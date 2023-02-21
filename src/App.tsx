ce52e4a9b5fabeb545f015d323c1124a9ce59bd9
Catalina Sarria <cimorenos@unal.edu.co>
2023-02-20 22:00:26 -0500

    Make App a component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,66 @@
  * App script to render main processes of the application.
  */
 
-import LogIn from "./components/login";
 import * as ReactDOM from 'react-dom/client';
+import React from "react";
+import LogIn from "./components/login";
 
-// Processes when the window has loaded its content.
-window.addEventListener('DOMContentLoaded', () => {
-    // Get root div.
-    let darkButton = ReactDOM.createRoot($("#root")[0]);
+interface IAppState {
+    isLogged: boolean;
+}
 
-    // Render login view when window is ready.
-    darkButton.render(<LogIn />);
 
-});
+/**
+ * Class that represent the App.
+ * @extends {React.Component}
+ */
+class App extends React.Component<{}, IAppState> {
+
+    /**
+     * Create the component.
+     * @param props {object} - Properties of the component.
+     */
+    constructor(props: {}) {
+
+        // Create superior class.
+        super(props);
+
+        // Bind actions.
+        this.handleLogIn = this.handleLogIn.bind(this);
+
+        // Set state.
+        this.state = {
+            isLogged: false,
+        }
+
+    }
+
+    /**
+     * Validate that the login info is correct.
+     * @param username {string} - Given username.
+     * @param password {string} - Password to ve validated.
+     */
+    handleLogIn(username: string, password: string) {
+        this.setState({
+            isLogged: true
+        });
+    }
+
+    /**
+     * Config the characteristics of the button according to theme and render
+     * the button.
+     * @returns {React.ReactNode} the button node.
+     */
+    render(): React.ReactNode {
+
+        return <LogIn />;
+
+    }
+}
+
+
+// Get root div.
+let darkButton = ReactDOM.createRoot($("#root")[0]);
+
+// Render login view when window is ready.
+darkButton.render(<App />);
